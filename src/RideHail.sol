619adadd0fb8328c0c900ef071ba9af334ace3bc
Dliteofficial <opeyemi4adewole@gmail.com>
2023-01-10 16:00:44 +0100

    Added Payment Functionality

--- src/RideHail.sol
+++ src/RideHail.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "src/DateTime.sol";
+import "src/libraries/DateTime.sol";
+import "src/libraries/AggregatorV3Interface.sol";
+import "src/libraries/IERC20.sol";
 
 contract RideHail is DateTime{
+
+    AggregatorV3Interface internal pricefeed;
+    address internal USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
 
     struct userDetail{
         bytes user_driver;
@@ -16,6 +21,10 @@ contract RideHail is DateTime{
     modifier onlyUser() {
         require(tx.origin == msg.sender, "Contracts can't order rides, where are they going?");
         _;
+    }
+
+    constructor() {
+        pricefeed = AggregatorV3Interface(0xfE4A8cc5b5B2366C1B58Bea3858e81843581b2F7); //USDC/USD
     }
 
     function registerUser(bool _user_driver) external onlyUser{
@@ -55,6 +64,18 @@ contract RideHail is DateTime{
         require(userDetails[_address].userName != address(0), "Caller doesn't own an account");
         _user_driver = abi.decode(userDetails[_address].user_driver, (bool)) == true ? "User" : "Driver";
         yearJoined = userDetails[_address].YearJoined;
+    }
+
+    function _getPrice() internal view returns (int) {
+        ( , int price, , , ) = pricefeed.latestRoundData();
+        return price / 1e8; //price was originally scaled up by 1e8 so we had to divide
+    }
+
+    function _makePayment(address _address, uint amount) internal {
+        require(IERC20(USDC).balanceOf(_address) >= amount, "Insufficient Balance");
+        uint balanceBefore = IERC20(USDC).balanceOf(address(this));
+        IERC20(USDC).transferFrom(_address,  (this), amount);
+        require(IERC20(USDC).balanceOf(address(this)) >= balanceBefore + amount, "Transfer Failed!");
     }
 
 }
